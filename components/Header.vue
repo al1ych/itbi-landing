efbd96b870f1a757d53e9843c22dbb776271aa01
al1ych <decter49pro@gmail.com>
2024-04-22 16:12:05 +0300

    добавил карусельку и хедер стоит на месте

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="flex justify-between items-center mx-16 mt-10 sticky">
+  <div class="flex justify-between items-center w-full">
     <div class="flex items-center">
       <Logo></Logo>
       <NavBar></NavBar>
     </div>
-    <ContactUsButton variant="outline" />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,55 @@
 import ContactUsButton from '../components/ContactUsButton.vue';
 <template>
-  <section class="bg-itbtPurple-600 w-full h-[3000px]">
-    <div class="flex justify-between w-full">
-      <div class="mx-40 mt-48 w-[50%]">
-        <h1 class="text-white text-7xl font-bold">
-          IT решения на <br />
-          1С и Битриксе
-        </h1>
+  <body class="text-white font-thin">
+    <section class="bg-itbtPurple-600 w-full h-screen" name="main">
+      <div class="flex justify-between w-full">
+        <div class="mx-40 mt-48 w-[50%]">
+          <h1 class="text-white text-6xl font-bold">
+            IT решения на <br />
+            1С и Битриксе
+          </h1>
 
-        <h2 class="text-[#EEE6FF] text-xl font-thin mt-8">
-          Мы специализируемся на разработке и внедрении систем ERP, CRM, ECM, а
-          также биллинговых систем для автоматизации ключевых бизнес-процессов
-        </h2>
+          <h2 class="text-[#EEE6FF] text-xl font-thin mt-8">
+            Мы специализируемся на разработке и внедрении систем ERP, CRM, ECM,
+            а также биллинговых систем для автоматизации ключевых
+            бизнес-процессов
+          </h2>
 
-        <ContactUsButton variant="solid" class="mt-10" />
+          <ContactUsButton variant="solid" class="mt-10" />
+        </div>
+
+        <div class="w-[50%] mr-24">
+          <img src="/illustrations/squiggle.svg" alt="squiggle" class="mt-40" />
+        </div>
       </div>
 
-      <div class="w-[50%] mr-24">
-        <img src="/illustrations/squiggle.svg" alt="squiggle" class="mt-40" />
+      <div class="w-full flex flex-col justify-center items-center mt-24">
+        Технологии
+        <TechnologyCarousel />
       </div>
-    </div>
-  </section>
+    </section>
+
+    <section class="bg-[#fcfaff] w-full h-[3000px]" name="projects">
+      <div class="flex justify-between w-full">
+        <div class="mx-40 mt-48 w-[50%]">
+          <h1 class="text-white text-7xl font-bold">
+            IT решения на <br />
+            1С и Битриксе
+          </h1>
+
+          <h2 class="text-[#EEE6FF] text-xl font-thin mt-8">
+            Мы специализируемся на разработке и внедрении систем ERP, CRM, ECM,
+            а также биллинговых систем для автоматизации ключевых
+            бизнес-процессов
+          </h2>
+
+          <ContactUsButton variant="solid" class="mt-10" />
+        </div>
+
+        <div class="w-[50%] mr-24">
+          <img src="/illustrations/squiggle.svg" alt="squiggle" class="mt-40" />
+        </div>
+      </div>
+    </section>
+  </body>
 </template>
