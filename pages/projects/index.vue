6fe3a9ecd04b5520fe244a69fddf9a7f6601eb7e
al1ych <decter49pro@gmail.com>
2024-04-30 21:21:51 +0300

    contacts section in projects page

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -8,5 +8,15 @@ import ContactUsButton from '../components/ContactUsButton.vue';
       alt="bg"
       class="w-full absolute pointer-events-none select-none -top-[600px] left-0"
     />
+
+    <div class="relative">
+      <img
+        src="/backgrounds/cutout-ellipse.svg"
+        alt="bg"
+        class="w-full absolute z-10 -mt-24 h-[30rem] pointer-events-none select-none"
+      />
+
+      <SectionContacts class="mt-48 absolute" />
+    </div>
   </body>
 </template>
